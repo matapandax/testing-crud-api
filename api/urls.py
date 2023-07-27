329eec0c88fa07ccf4a7988806e554bcfeb9bf22
matapandax <caesariosatriatama@gmail.com>
2023-07-27 12:02:59 +0700

    Pesan commit

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.index, name="/"),
     path('student-view/<str:pk>/', views.studentView, name="studentview"),
     path('add-student/', views.studentAdd, name="studentadd"),
+    path('update-student/<str:pk>/', views.studentUpdate, name="studentupdate"),
+    path('delete-student/<str:pk>/', views.studentdelete, name="studentdelete")
 ]
--- api/views.py
+++ api/views.py
@@ -9,38 +9,57 @@ from .serializer import StudentSerializer
 
 @api_view(['GET'])
 def index(request):
-    students = Student.objects.all()
-    serialstudents = StudentSerializer(students, many=True)
-    return Response({
-        'status':200,
-        'students':serialstudents.data
-    })
+    student = Student.objects.all()
+    serialstudents = StudentSerializer(student, many=True)
+    return Response(serialstudents.data)
 
-@api_view(['GET'])
-def studentView(request, pk):
+api_view(['GET'])
+def studentView(request,pk):
+    student = Student.object.get(id=pk)
+    serialstudent = StudentSerializer(student, many=False)
+    return Response(serialstudent.data)
+
+api_view(['POST'])
+def studentAdd(request):
+    serialdata= StudentSerializer(data=request.data)
+    if serialdata.is_valid():
+        serialdata.save()
+
+    return Response(serialdata.data)
+        
+@api_view(['POST'])
+def studentUpdate(request, pk):
     try :
         student = Student.objects.get(id=pk)
-        serialstudent = StudentSerializer(student, many=False)
+        serialstudent = StudentSerializer(instance=student, data=request.data)
+
+        if serialstudent.is_valid():
+            serialstudent.save()
+            
         return Response({
             'status':200,
-            'students':serialstudent.data,
+            'student':serialstudent.data,
+            'message':'Updated successfully'
         })
+
     except :
         return Response({'status':400})
 
 
-@api_view(['POST'])
-def studentAdd(request):
+@api_view(['DELETE'])
+def studentdelete(request, pk):
     try:
-            
-        serialdata = StudentSerializer(data=request.data)
-        if serialdata.is_valid():
-            serialdata.save()
+
+        student = Student.objects.get(id=pk)
+        student.delete()
+        
+        students = Student.objects.all()
+        serialstudents = StudentSerializer(students, many=True)
         
         return Response({
             'status':200,
-            'student':serialdata.data,
-            'message':'Student added successfully'
+            'student':serialstudents.data,
+            'message':'Student Deleted successfully'
         })
 
     except:
